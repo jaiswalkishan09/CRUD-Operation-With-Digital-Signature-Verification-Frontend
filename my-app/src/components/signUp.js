1230808ef9624208bf9c90ad439a2df0559f59d7
Kishan Jaiswal <jaiswalkishan09@gmail.com>
2022-10-09 16:33:32 +0545

    signup

--- my-app/src/components/signUp.js
+++ my-app/src/components/signUp.js
@@ -1,19 +1,21 @@
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import { useEffect, useState } from 'react';
+import Cookies from 'universal-cookie';
+import { useState } from 'react';
 
 function SignUp() {
     const [firstName,setFirstName]=useState("");
     const[firstNameCheck,setFirstNameCheck]=useState(true);
     const [lastName,setLastName]=useState("");
     const[lastNameCheck,setLastNameCheck]=useState(true);
-    const [emai,setEmail]=useState("");
+    const [email,setEmail]=useState("");
     const[emailCheck,setEmailCheck]=useState(true);
     const [mobile,setMobile]=useState("");
     const[mobileCheck,setMobileCheck]=useState(true);
     const [password,setPassword]=useState("");
     const[passwordCheck,setPasswordCheck]=useState(true);
+    const [loader,setLoader]=useState(false);
 
     const firstNameSet=(e)=>{
         setFirstName(e.target.value);
@@ -43,21 +45,45 @@ function SignUp() {
         setPassword(e.target.value);
        e.target.value && e.target.value.length>=8?setPasswordCheck(false):setPasswordCheck(true);
     }
-
-
-    // useEffect(async()=>{
-    //     try{
-            
-    //     }
-    //     catch(e)
-    //     {
-    //         console.log(e);
-    //     }
-
-    // },[firstName])
+    const createUser=async () => {
+        try {
+        if(!firstNameCheck && !lastNameCheck && !emailCheck && !mobileCheck && !passwordCheck)
+        {
+            setLoader(true);
+            let data={
+                "firstName":firstName,
+                "lastName":lastName,
+                "email":email,
+                "mobileNo":mobile,
+                "password":password
+            }
+            const requestOptions = {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(data)
+            };
+            const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/users/signup`,requestOptions)
+            console.log(response.status)
+            let json_res = await response.json();
+            if (response.status==201) {
+                const cookies = new Cookies();
+                cookies.set('token',json_res.token);
+                cookies.set('publicKey',json_res.publicKey);
+                cookies.set('privateKey',json_res.privateKey);
+                setLoader(false);
+            }
+        }
+        else{
+            alert("Please sign up all the fields with valid data.")
+        }
+        } catch (err) {
+            setLoader(false);
+            alert("Something went wrong please try again.")
+        }
+      };
     return(
         <Container id="main-container" className='d-grid h-100'>
-            <Form id="sign-up-form" className="w-100">
+            {!loader?<Form id="sign-up-form" className="w-100">
                 <h1>Please Sign Up</h1>
                 <Form.Group>
                     <Form.Label className="text-left">First Name:</Form.Label>
@@ -95,9 +121,9 @@ function SignUp() {
                     </Form.Text>:""}
                 </Form.Group>
                 <div className='d-grid mt-3'>
-                    <Button variant='primary'>Sign Up</Button>
+                    <Button variant='primary' onClick={createUser}>Sign Up</Button>
                 </div>
-            </Form>
+            </Form>:"Please Wait..."}
         </Container>
     )
 }
